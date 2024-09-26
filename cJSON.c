b2fb03acb82ef75d5b4efca9ab658c3c81e167d1
wellwei <y1827104243@gmail.com>
2024-09-26 21:23:36 +0800

    feat: 添加test.cpp文件和相关功能实现

--- cJSON.c
+++ cJSON.c
@@ -1,225 +1,162 @@
-#include <stdlib.h>
-#include <string.h>
+/*
+  Copyright (c) 2009 Dave Gamble
 
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+  THE SOFTWARE.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
 #include "cJSON.h"
 
-CJSON_PUBLIC(void) cJSON_InitHooks(cJSON_Hooks *hooks) {
-    if (!hooks) {
-        memset(&global_hooks, '\0', sizeof(cJSON_Hooks));
-    } else {
-        global_hooks = *hooks;
+/* Parse text to JSON, then render back to text, and print! */
+void doit(char *text)
+{
+    char *out;cJSON *json;
+
+    json=cJSON_Parse(text);
+    if (!json) {printf("Error before: [%s]\n",cJSON_GetErrorPtr());}
+    else
+    {
+        out=cJSON_Print(json);
+        cJSON_Delete(json);
+        printf("%s\n",out);
+        free(out);
     }
 }
 
-static unsigned char* cJSON_strdup(const unsigned char* string, const cJSON_Hooks * const hook) {
-    size_t length = 0;
-    unsigned char *copy = NULL;
+/* Read a file, parse, render back, etc. */
+void dofile(char *filename)
+{
+    FILE *f;long len;char *data;
 
-    if (string == NULL) {
-        return NULL;
-    }
-
-    length = strlen((const char*)string) + sizeof("");
-    copy = (unsigned char*)hook->malloc_fn(length);
-
-    if (copy == NULL) {
-        return NULL;
-    }
-
-    memcpy(copy, string, length);
-
-    return copy;
+    f=fopen(filename,"rb");fseek(f,0,SEEK_END);len=ftell(f);fseek(f,0,SEEK_SET);
+    data=(char*)malloc(len+1);fread(data,1,len,f);fclose(f);
+    doit(data);
+    free(data);
 }
 
-CJSON_PUBLIC(cJSON *) cJSON_New_Item(const cJSON_Hooks * const hooks) {
-    cJSON *node = (cJSON *)hooks->malloc_fn(sizeof(cJSON));
-    if (node) {
-        memset(node, '\0', sizeof(cJSON));
+/* Used by some code below as an example datatype. */
+struct record {const char *precision;double lat,lon;const char *address,*city,*state,*zip,*country; };
+
+/* Create a bunch of objects as demonstration. */
+void create_objects()
+{
+    cJSON *root,*fmt,*img,*thm,*fld;char *out;int i;	/* declare a few. */
+    /* Our "days of the week" array: */
+    const char *strings[7]={"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
+    /* Our matrix: */
+    int numbers[3][3]={{0,-1,0},{1,0,0},{0,0,1}};
+    /* Our "gallery" item: */
+    int ids[4]={116,943,234,38793};
+    /* Our array of "records": */
+    struct record fields[2]={
+            {"zip",37.7668,-1.223959e+2,"","SAN FRANCISCO","CA","94107","US"},
+            {"zip",37.371991,-1.22026e+2,"","SUNNYVALE","CA","94085","US"}};
+
+    /* Here we construct some JSON standards, from the JSON site. */
+
+    /* Our "Video" datatype: */
+    root=cJSON_CreateObject();
+    cJSON_AddItemToObject(root, "name", cJSON_CreateString("Jack (\"Bee\") Nimble"));
+    cJSON_AddItemToObject(root, "format", fmt=cJSON_CreateObject());
+    cJSON_AddStringToObject(fmt,"type",		"rect");
+    cJSON_AddNumberToObject(fmt,"width",		1920);
+    cJSON_AddNumberToObject(fmt,"height",		1080);
+    cJSON_AddFalseToObject (fmt,"interlace");
+    cJSON_AddNumberToObject(fmt,"frame rate",	24);
+
+    out=cJSON_Print(root);	cJSON_Delete(root);	printf("%s\n",out);	free(out);	/* Print to text, Delete the cJSON, print it, release the string. */
+
+    /* Our "days of the week" array: */
+    root=cJSON_CreateStringArray(strings,7);
+
+    out=cJSON_Print(root);	cJSON_Delete(root);	printf("%s\n",out);	free(out);
+
+    /* Our matrix: */
+    root=cJSON_CreateArray();
+    for (i=0;i<3;i++) cJSON_AddItemToArray(root,cJSON_CreateIntArray(numbers[i],3));
+
+/*	cJSON_ReplaceItemInArray(root,1,cJSON_CreateString("Replacement")); */
+
+    out=cJSON_Print(root);	cJSON_Delete(root);	printf("%s\n",out);	free(out);
+
+
+    /* Our "gallery" item: */
+    root=cJSON_CreateObject();
+    cJSON_AddItemToObject(root, "Image", img=cJSON_CreateObject());
+    cJSON_AddNumberToObject(img,"Width",800);
+    cJSON_AddNumberToObject(img,"Height",600);
+    cJSON_AddStringToObject(img,"Title","View from 15th Floor");
+    cJSON_AddItemToObject(img, "Thumbnail", thm=cJSON_CreateObject());
+    cJSON_AddStringToObject(thm, "Url", "http:/*www.example.com/image/481989943");
+    cJSON_AddNumberToObject(thm,"Height",125);
+    cJSON_AddStringToObject(thm,"Width","100");
+    cJSON_AddItemToObject(img,"IDs", cJSON_CreateIntArray(ids,4));
+
+    out=cJSON_Print(root);	cJSON_Delete(root);	printf("%s\n",out);	free(out);
+
+    /* Our array of "records": */
+
+    root=cJSON_CreateArray();
+    for (i=0;i<2;i++)
+    {
+        cJSON_AddItemToArray(root,fld=cJSON_CreateObject());
+        cJSON_AddStringToObject(fld, "precision", fields[i].precision);
+        cJSON_AddNumberToObject(fld, "Latitude", fields[i].lat);
+        cJSON_AddNumberToObject(fld, "Longitude", fields[i].lon);
+        cJSON_AddStringToObject(fld, "Address", fields[i].address);
+        cJSON_AddStringToObject(fld, "City", fields[i].city);
+        cJSON_AddStringToObject(fld, "State", fields[i].state);
+        cJSON_AddStringToObject(fld, "Zip", fields[i].zip);
+        cJSON_AddStringToObject(fld, "Country", fields[i].country);
     }
-    return node;
+
+/*	cJSON_ReplaceItemInObject(cJSON_GetArrayItem(root,1),"City",cJSON_CreateIntArray(ids,4)); */
+
+    out=cJSON_Print(root);	cJSON_Delete(root);	printf("%s\n",out);	free(out);
+
 }
 
-/**
- * @brief 释放 cJSON 节点 , 常量字符串不释放
- * 
- * @param c cJSON 节点
- */
-void cJSON_Delete(cJSON *c) {
-    cJSON *next = NULL;
-    while (c != NULL) {
-        next = c->next;
-        if (!(c->type & cJSON_IsReference) && (c->child != NULL)) {
-            cJSON_Delete(c->child);
-        }
-        if (!(c->type & cJSON_IsReference) && (c->valuestring != NULL)) {
-            global_hooks.free_fn(c->valuestring);
-        }
-        if (!(c->type & cJSON_StringIsConst) && (c->string != NULL)) {
-            global_hooks.free_fn(c->string);
-        }
-        global_hooks.free_fn(c);
-        c = next;
-    }
+int main (int argc, const char * argv[]) {
+    /* a bunch of json: */
+    char text1[]="{\n\"name\": \"Jack (\\\"Bee\\\") Nimble\", \n\"format\": {\"type\":       \"rect\", \n\"width\":      1920, \n\"height\":     1080, \n\"interlace\":  false,\"frame rate\": 24\n}\n}";
+    char text2[]="[\"Sunday\", \"Monday\", \"Tuesday\", \"Wednesday\", \"Thursday\", \"Friday\", \"Saturday\"]";
+    char text3[]="[\n    [0, -1, 0],\n    [1, 0, 0],\n    [0, 0, 1]\n	]\n";
+    char text4[]="{\n		\"Image\": {\n			\"Width\":  800,\n			\"Height\": 600,\n			\"Title\":  \"View from 15th Floor\",\n			\"Thumbnail\": {\n				\"Url\":    \"http:/*www.example.com/image/481989943\",\n				\"Height\": 125,\n				\"Width\":  \"100\"\n			},\n			\"IDs\": [116, 943, 234, 38793]\n		}\n	}";
+    char text5[]="[\n	 {\n	 \"precision\": \"zip\",\n	 \"Latitude\":  37.7668,\n	 \"Longitude\": -122.3959,\n	 \"Address\":   \"\",\n	 \"City\":      \"SAN FRANCISCO\",\n	 \"State\":     \"CA\",\n	 \"Zip\":       \"94107\",\n	 \"Country\":   \"US\"\n	 },\n	 {\n	 \"precision\": \"zip\",\n	 \"Latitude\":  37.371991,\n	 \"Longitude\": -122.026020,\n	 \"Address\":   \"\",\n	 \"City\":      \"SUNNYVALE\",\n	 \"State\":     \"CA\",\n	 \"Zip\":       \"94085\",\n	 \"Country\":   \"US\"\n	 }\n	 ]";
+
+    /* Process each json textblock by parsing, then rebuilding: */
+    doit(text1);
+    doit(text2);
+    doit(text3);
+    doit(text4);
+    doit(text5);
+
+    /* Parse standard testfiles: */
+/*	dofile("../../tests/test1"); */
+/*	dofile("../../tests/test2"); */
+/*	dofile("../../tests/test3"); */
+/*	dofile("../../tests/test4"); */
+/*	dofile("../../tests/test5"); */
+
+    /* Now some samplecode for building objects concisely: */
+    create_objects();
+
+    return 0;
 }
-
-CJSON_PUBLIC(cJSON *) cJSON_CreateObject(void) {
-    cJSON *item = cJSON_New_Item(&global_hooks);
-    if (item) {
-        item->type = cJSON_Object;
-    }
-    return item;
-}
-
-CJSON_PUBLIC(cJSON *) cJSON_CreateTrue(void) {
-    cJSON *item = cJSON_New_Item(&global_hooks);
-    if (item) {
-        item->type = cJSON_True;
-    }
-    return item;
-}
-
-CJSON_PUBLIC(cJSON *) cJSON_CreateFalse(void) {
-    cJSON *item = cJSON_New_Item(&global_hooks);
-    if (item) {
-        item->type = cJSON_False;
-    }
-    return item;
-}
-
-CJSON_PUBLIC(cJSON *) cJSON_CreateNull(void) {
-    cJSON *item = cJSON_New_Item(&global_hooks);
-    if (item) {
-        item->type = cJSON_NULL;
-    }
-    return item;
-}
-
-CJSON_PUBLIC(cJSON *) cJSON_CreateNumber(double num) {
-    cJSON *item = cJSON_New_Item(&global_hooks);
-    if (item) {
-        item->type = cJSON_Number;
-        item->valuedouble = num;
-        item->valueint = (int)num;
-    }
-    return item;
-}
-
-CJSON_PUBLIC(cJSON *) cJSON_CreateString(const char *string) {
-    cJSON *item = cJSON_New_Item(&global_hooks);
-    if (item) {
-        item->type = cJSON_String;
-        item->valuestring = (char*)cJSON_strdup((const unsigned char*)string, &global_hooks);
-        if (item->valuestring == NULL) {
-            cJSON_Delete(item);
-            return NULL;
-        }
-    }
-    return item;
-}
-
-CJSON_PUBLIC(cJSON *) cJSON_CreateArray(void) {
-    cJSON *item = cJSON_New_Item(&global_hooks);
-    if (item) {
-        item->type = cJSON_Array;
-    }
-    return item;
-}
-
-static void suffix_object(cJSON *prev, cJSON *item) {
-    prev->next = item;
-    item->prev = prev;
-}
-
-CJSON_PUBLIC(cJSON *) cJSON_CreateStringArray(const char **strings, int count) {
-    size_t i = 0;
-    cJSON *n = NULL;    // next
-    cJSON *p = NULL;    // prev
-    cJSON *a = NULL;   // array
-
-    if ((count < 0) || (strings == NULL)) {
-        return NULL;
-    }
-
-    a = cJSON_CreateArray();
-
-    for (i = 0; a && (i < (size_t)count); i++) {
-        n = cJSON_CreateString(strings[i]);
-        if (n == NULL) {
-            cJSON_Delete(a);    // 创建失败，释放 a
-            return NULL;
-        }
-        if (i == 0) {
-            a->child = n;   // 第一个节点，将 n 赋值给 a->child
-        }
-        else {
-            suffix_object(p, n);    // 将 n 追加到 p 后面
-        }
-        p = n;  // p 指向 n
-    }
-    return a;
-}
-
-/**
- * @brief 将 item 添加到 obj 的 child 链中
- * @retval 1：成功
- */
-static cJSON_bool add_item_to_list(cJSON *list, cJSON *item) {
-    cJSON *child = NULL;
-
-    if ((item == NULL) || (list == NULL)) {
-        return 0;
-    }
-
-    child = list->child;
-    if (child == NULL) {
-        list->child = item;    // array 没有子节点，直接将 item 赋值给 array->child
-    } else {
-        while (child && child->next) {  // 将 child 移动到链表的最后一个节点
-            child = child->next;
-        }
-        suffix_object(child, item);     // 将 item 追加到 child 后面
-    }
-}
-
-/**
- * @brief 将 item 以 string 为 key 添加到 object 中
- * @note item 可以是 任意 cJSON 类型
- * 
- * @param object 
- * @param string 
- * @param item 
- * @param hooks 
- * @param constant_key key 是否是外部定义的常量
- * @return cJSON_bool 
- * @retval 1：成功
- */
-static cJSON_bool add_item_to_object(cJSON *object, const char * const string, cJSON * const item, const cJSON_Hooks * const hooks, const cJSON_bool constant_key) {
-    char *new_key = NULL;
-    int new_type = cJSON_String;
-
-    if ((object == NULL) || (string == NULL) || (item == NULL)) {
-        return 0;
-    }
-
-    // 如果 key 是常量，直接赋值给 new_key
-    if (constant_key) {
-        new_key = (char*)string;    // key 是常量
-        new_type = item->type | cJSON_StringIsConst;
-    } else {
-        new_key = (char*)cJSON_strdup((const unsigned char*)string, hooks);
-        if (new_key == NULL) {
-            return 0;
-        }
-        new_type = item->type & ~cJSON_StringIsConst;   // key 不是常量
-    }
-
-    if (!(item->type & cJSON_StringIsConst) && (item->string != NULL)) {    // item->string 不是常量，释放 item->string
-        hooks->free_fn(item->string);
-    }
-    
-    item->string = new_key;
-    item->type = new_type;
-
-    return add_item_to_list(object, item);   // 将 item 添加到 object 中
-}
-
